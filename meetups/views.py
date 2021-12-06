0e813be2ec130f5621960bf735d48b2dd6a0282e
Aryan <59768317+Aryanx17@users.noreply.github.com>
2021-12-06 05:39:24 +0530

    'dsite'

---
+++ meetups/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello World!')
+
+def i2(request):
+    return HttpResponse('Home Page')
